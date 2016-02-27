b663071e610457ab0e01584e74c46b63a868e4f6
Pangyu Teng <pteng@medqia.com>
2016-02-27 12:37:34 -0800

    + added conf which links to scripts folder + refactored train_reports.py

---
+++ voxnet/conf.py
@@ -0,0 +1,9 @@
+import os
+
+ROOT_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SCRIPT_PATH = os.path.join(ROOT_PATH,'scripts')
+
+TRAIN_PY =  os.path.join(SCRIPT_PATH,'train.py')
+TEST_PY =  os.path.join(SCRIPT_PATH,'test.py')
+VIZORTHO_PY = os.path.join(SCRIPT_PATH,'output_viz_ortho.py')
+TRAINREPORT_PY  = os.path.join(SCRIPT_PATH,'train_reports.py')
